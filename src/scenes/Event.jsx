77021ecc4cf459036026b65e349d847c7903a9ca
Reshan Gayantha <reshangayantha9@gmail.com>
2024-04-15 22:06:32 +0530

    updated

--- src/scenes/Event.jsx
+++ src/scenes/Event.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import { motion } from "framer-motion";
+import LineGradient from '../components/LineGradient';
 function Event() {
   return (
-    <>
+    <div className=" mt-[70px]">
+    <p className="font-playfair font-semibold text-4xl mb-3">
+            AWARENESS <span className="text-red">SESSION 1</span>
+          </p>
+          <LineGradient width="w-1/3" />
+          <p className="font-playfair font-semibold text-lg mt-8 uppercase text-center">
+           <span className='text-xl'>Best Practices in Battlebot design</span>  <br/>
+            21st April 2024 <br /> 6.00 P.M. Onwards
+          </p>
     <div className="flex justify-between items-center flex-col">
+    
         <motion.div
         className="md:w-full flex justify-center items-center mt-10"
         initial="hidden"
@@ -15,18 +25,19 @@ function Event() {
           visible: { opacity: 1, x: 0 },
         }}
       >
+        
         <div className="md:w-[50%]">
-            <img src="assets/session1.jpg" alt="" srcset="" className='w-[100%] md:mt-14'/>
+            <img src="assets/session1.jpg" alt="" srcset="" className='w-[100%]'/>
         </div>
       </motion.div>
         <a href="https://"target="_blank"rel="noopener noreferrer" className='text-center'>
               <button className="custom-btn btn-11">
-                Join with us<div className="dot"></div>
+                Join via zoom<div className="dot"></div>
               </button>
             </a>
 
     </div>
-        </>
+        </div>
   )
 }
 
