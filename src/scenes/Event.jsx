9ad4d13f2683064f753d98be83fc51af38104d6b
Reshan Gayantha <reshangayantha9@gmail.com>
2024-04-16 08:54:40 +0530

    updated

--- src/scenes/Event.jsx
+++ src/scenes/Event.jsx
@@ -1,16 +1,29 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import LineGradient from '../components/LineGradient';
+import { useState } from 'react';
+import { useEffect } from 'react';
 function Event() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const targetDateTime = new Date('2024-04-21T16:00:00');
+  useEffect(() => {
+    const checkDateTime = () => {
+      const currentDate = new Date();
+      setIsEnabled(currentDate >= targetDateTime);
+    };
+    checkDateTime();
+    const intervalId = setInterval(checkDateTime, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
   return (
     <div className=" mt-[70px]">
     <p className="font-playfair font-semibold text-4xl mb-3">
             AWARENESS <span className="text-red">SESSION 1</span>
           </p>
           <LineGradient width="w-1/3" />
-          <p className="font-playfair font-semibold text-lg mt-8 uppercase text-center">
-           <span className='text-xl'>Best Practices in Battlebot design</span>  <br/>
-            21st April 2024 <br /> 6.00 P.M. Onwards
+          <p className="font-playfair font-semibold text-lg mt-10 uppercase ">
+           <span className='text-2xl'>topic : Best Practices in Battlebot design</span>  <br/>
+            date : 21st April 2024 <br />time : 6.00 P.M. Onwards
           </p>
     <div className="flex justify-between items-center flex-col">
     
@@ -31,7 +44,7 @@ function Event() {
         </div>
       </motion.div>
         <a href="https://"target="_blank"rel="noopener noreferrer" className='text-center'>
-              <button className="custom-btn btn-11">
+              <button className="custom-btn btn-11"disabled={!isEnabled}>
                 Join via zoom<div className="dot"></div>
               </button>
             </a>
