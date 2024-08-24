c7f5b190e68c70c75ec6291916369b0d10229ded
Reshan Gayantha <reshangayantha9@gmail.com>
2024-08-24 20:11:16 +0530

    Updated

--- src/scenes/Register.jsx
+++ src/scenes/Register.jsx
@@ -5,35 +5,6 @@ import "../index.css";
 import "./countdown.css";
 
 const Register = () => {
-  // const inputDate = "03 AUGUST 2024";
-  // const [days, setDays] = useState(0);
-  // const [hours, setHours] = useState(0);
-  // const [minutes, setMinutes] = useState(0);
-  // const [seconds, setSeconds] = useState(0);
-  // const [inputDate, setInputDate] = useState("25 MARCH 2024");
-  // const [currentDate, setCurrentDate] = useState(inputDate);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentDate(new Date());
-    }, 1000);
-    return () => clearInterval(intervalId);
-  }, []);
-  useEffect(() => {
-    const changingDate = new Date(inputDate);
-    const currentDate = new Date();
-    const totalSeconds = (changingDate - currentDate) / 1000;
-
-    setDays(formatTime(Math.floor(totalSeconds / 3600 / 24)));
-    setHours(Math.floor(totalSeconds / 3600) % 24);
-    setMinutes(Math.floor(totalSeconds / 60) % 60);
-    setSeconds(Math.floor(totalSeconds % 60));
-  }, [inputDate, currentDate]);
-
-  function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
-  }
-
   return (
     <section id="sessions" className="mt-32">
       <div className="md:flex md:justify-between md:gap-16">
